55e4efe5bf01fe1ed39c1c974559c697d40cfcd3
Lam Jun Rong <root@jro.sg>
2026-02-10 10:07:07 +0800

    Enhance thunk nuker

--- examples/passives/thunk_nuker.py
+++ examples/passives/thunk_nuker.py
@@ -1,11 +1,18 @@
 from __future__ import annotations
 from herapi import *
 import ida_funcs
+import ida_bytes
+
+
+def is_extern(f: ida_funcs.func_t) -> bool:
+	if f.size() > 0x10:
+		return False
+	return len(set(ida_bytes.get_bytes(f.start_ea, f.size()))) < 2
+
 
 class ReplacingScheme(Scheme):
 	def __init__(self):
 		pattern = CallPat(None, ignore_arguments=True, bind_name="func")
-		print(pattern)
 		super().__init__(pattern)
 
 	def on_matched_item(self, item, ctx: MatchContext) -> ASTPatch|None:
@@ -18,6 +25,9 @@ class ReplacingScheme(Scheme):
 		if not thunk or len(thunk) != 2:
 			return None
 		thunk = thunk[0]
+		thunk_func = ida_funcs.get_func(thunk)
+		if not thunk_func or is_extern(thunk_func):
+			return None
 		func_expr.x.obj_ea = thunk
 		
 register_storage_scheme("thunk_nuker", ReplacingScheme())
